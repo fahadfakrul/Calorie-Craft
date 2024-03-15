903691c2dcb8c14787b1a315fa0eed9540dbd622
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-16 00:12:39 +0600

    tailwind setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,16 @@ function App() {
     <>
       
       <h1 className='text-6xl bg-red-500'>Vite + React</h1>
+      <div className="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
      
     </>
   )
